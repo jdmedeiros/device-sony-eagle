72e0a84e29b10df071953cd412fce6ac38f101cf
J. Daniel Medeiros <jdmedeiros@gmail.com>
2015-03-19 13:00:09 -0100

    Sony eagle device tree after merging yukon into it

---
+++ README.md
@@ -0,0 +1,7 @@
+Copyright (C) Sony Mobile Communications 2014
+=============================================
+
+This is the Android device configuration for Xperia M2 (yukon platform). I modified it so that it would not need the yukon device tree.
+
+Build instructions
+http://developer.sonymobile.com/knowledge-base/open-source/open-devices/aosp-build-instructions/
---
+++ aosp_d2303.mk
@@ -0,0 +1,233 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_KERNEL_CONFIG := aosp_yukon_eagle_defconfig
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+
+# Include others
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+
+######################################################################
+# 
+# MERGE
+#
+######################################################################
+
+SOMC_PLATFORM := yukon
+
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/eagle/overlay
+
+SONY_ROOT:=device/sony/eagle/rootdir
+
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/ueventd.yukon.rc:root/ueventd.yukon.rc \
+    $(SONY_ROOT)/init.yukon.rc:root/init.yukon.rc \
+    $(SONY_ROOT)/init.yukon.usb.rc:root/init.yukon.usb.rc \
+    $(SONY_ROOT)/init.yukon.pwr.rc:root/init.yukon.pwr.rc \
+    $(SONY_ROOT)/system/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(SONY_ROOT)/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(SONY_ROOT)/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(SONY_ROOT)/system/etc/init.yukon.bt.sh:system/etc/init.yukon.bt.sh \
+    $(SONY_ROOT)/system/etc/sec_config:system/etc/sec_config \
+    $(SONY_ROOT)/system/etc/gps.conf:system/etc/gps.conf
+ 
+# Copy extra files
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/system/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    $(SONY_ROOT)/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    $(SONY_ROOT)/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    $(SONY_ROOT)/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# NFC
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+
+#Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.primary.msm8226 \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    libacdbloader \
+    libacdbmapper \
+    libaudcal \
+    libaudioalsa \
+    libdiag
+
+# for audio.primary.msm8226
+PRODUCT_PACKAGES += \
+    libtinyalsa \
+    libtinycompress \
+    libaudioroute
+
+# Audio effects
+PRODUCT_PACKAGES += \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libqcomvoiceprocessingdescriptors \
+    libqcompostprocbundle
+
+#GFX
+PRODUCT_PACKAGES += \
+    gralloc.msm8226 \
+    copybit.msm8226 \
+    hwcomposer.msm8226 \
+    memtrack.msm8226 \
+    libgenlock \
+    libqdutils \
+    libqdMetaData
+
+#OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libstagefrighthw \
+    libOmxCore \
+    libmm-omxcore \
+    libOmxVdec \
+    libOmxVdecHevc \
+    libOmxVenc
+
+#lights
+PRODUCT_PACKAGES += \
+    lights.yukon
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag \
+    nfc_nci.pn54x.default
+
+#GPS
+PRODUCT_PACKAGES += \
+    libloc_api_v02 \
+    libloc_adapter \
+    libloc_eng \
+    libgps.utils \
+    gps.msm8226
+
+#Wifi
+PRODUCT_PACKAGES += \
+    hostapd \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+#Misc
+PRODUCT_PACKAGES += \
+    libmiscta \
+    libta \
+    tad_static \
+    rmt_storage \
+    ta_qmi_service \
+    ta2bin
+
+PRODUCT_PACKAGES += \
+    rmt_storage
+
+#OSS
+PRODUCT_PACKAGES += \
+    thermanager \
+    wcnss_addr \
+    bt_addr
+
+#Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory
+
+PRODUCT_PACKAGES += \
+    Dialer \
+    Email \
+    Exchange2 \
+    InCallUI \
+    Launcher3
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    e2fsck
+
+# Get the long list of apns
+PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Platform specific properties
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.bt.hci_transport=smd
+
+# Platform specific default properties
+#
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+######################################################################
+# 
+#  END OF MERGE
+#
+######################################################################
+
+$(call inherit-product, vendor/sony/eagle/eagle-vendor.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/eagle/overlay
+
+PRODUCT_COPY_FILES += \
+    device/sony/eagle/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/sony/eagle/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    device/sony/eagle/rootdir/system/etc/sap.conf:system/etc/sap.conf \
+    device/sony/eagle/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/sony/eagle/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/sony/eagle/rootdir/fstab.yukon:root/fstab.yukon \
+    device/sony/eagle/rootdir/init.yukon.dev.rc:root/init.yukon.dev.rc
+
+# Product attributes
+PRODUCT_NAME := aosp_d2303
+PRODUCT_DEVICE := eagle
+PRODUCT_MODEL := Xperia M2 (AOSP)
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_LOCALES += xhdpi hdpi
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240
